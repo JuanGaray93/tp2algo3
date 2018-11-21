dd32702f619c3b930424acb30eb23a1c9977ff35
Juan <juangaray93@gmail.com>
2018-11-21 19:47:18 -0300

    Revert "Agrego modificaciones en crearEntidadesIniciales"
    This reverts commit 54f48d35c3d43523515673042483c3f194a7b980.

--- src/com/company/modelo/Jugador.java
+++ src/com/company/modelo/Jugador.java
@@ -1,9 +1,6 @@
 package com.company.modelo;
 
-import com.company.excepciones.CasilleroLlenoException;
-import com.company.excepciones.CasilleroNoExistenteException;
-import com.company.excepciones.MontoErroneoError;
-import com.company.excepciones.OroInsuficienteException;
+import com.company.excepciones.*;
 import com.company.modelo.edificios.Castillo;
 import com.company.modelo.edificios.Edificio;
 import com.company.modelo.edificios.PlazaCentral;
@@ -11,113 +8,104 @@ import com.company.modelo.terreno.Mapa;
 import com.company.modelo.unidades.Aldeano;
 import com.company.modelo.unidades.Unidad;
 
+import javax.naming.LimitExceededException;
 import java.util.ArrayList;
 
 public class Jugador {
 
-    private final Integer LIMITE_POBLACIONAL = 50;
+	private final Integer LIMITE_POBLACIONAL = 50 ;
+	
+	ArrayList <Unidad> poblacion;
+	ArrayList <Edificio> edificios;
+	private static Integer oro;
 
-    ArrayList<Unidad> poblacion;
-    ArrayList <Edificio> edificios;
-    private static Integer oro;
+	public Jugador() {
+		this.oro = 100;
+	}
+	/*
+	public void mover(Unidad unidad, Integer x, Integer y) throws CasilleroNoExistenteException {
+		try {
+			unidad.moverA(x,y);
+		} catch (CasilleroLlenoException e) {
+			// TODO si el casillero esta lleno...
+		}
+	}*/
 
-    public Jugador(Mapa mapa) throws CasilleroLlenoException {
-        this.oro = 100;
-    }
+	public Integer getOro(){
+		return this.oro;
+	}
 
-    public void mover(Unidad unidad, Integer coordenadaHorizontal, Integer coordenadaVertical) throws CasilleroNoExistenteException {
-        try {
-            unidad.moverA(coordenadaHorizontal, coordenadaVertical);
-        } catch (CasilleroLlenoException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+	public void agregarAPoblacion(Unidad unidad) {
 
-    public Integer getOro(){
-        return this.oro;
-    }
+		if(poblacion.size() == LIMITE_POBLACIONAL) {
+			throw new LimitePoblacionalException("Alcanzaste el limite permitido de unidades");
+		}
+		poblacion.add(unidad);
+	}
+	
+	public void eliminarDePoblacion(Unidad unidad) {
+		if(!poblacion.contains(unidad)){
+			throw new UnidadInexistenteEnPoblacionException("Disculpe, la unidad no existe en la poblacion");
+		}
+		poblacion.remove(unidad);
 
-    public void agregarAPoblacion(Unidad unidad) {
+	}
 
-        if(poblacion.size() == LIMITE_POBLACIONAL) {
-            throw new LimitePoblacionalException("Alcanzaste el limite permitido de unidades");
-        }
+	public void eliminarDeConstrucciones(Edificio edificio) {
+		if(!edificios.contains(edificio)){
 
-        poblacion.add(unidad);
-    }
+			throw new EdificioInexistenteEnConstruccionesException("No existe tal edificio");
+		}
+		edificios.remove(edificio);
+	}
 
-    public void eliminarDePoblacion(Unidad unidad) {
+	public void sumarOro(Integer produccionOro) {
+		this.oro += produccionOro;
 
-        if(!poblacion.contains(unidad)){
-            throw new UnidadInexistenteEnPoblacionException("Disculpe, la unidad no existe en la poblacion");
-        }
+	}
 
-        poblacion.remove(unidad);
-    }
+	public void cobrar(Integer monto) throws OroInsuficienteException{
+		if(monto < 0){
+			throw new OroInsuficienteException("Se intentó hacer un cobro negativo. Algo salió horriblemente mal.");
+		}else
+			if (oro - monto < 0){
+				throw new OroInsuficienteException("Se intentó hacer un cobro negativo. Algo salió horriblemente mal.");
+			}
 
-    public boolean tieneOro(Integer oro) {
-        return this.oro == oro;
-    }
+		oro -= monto;
+	}
+	/*
+	public void actualizar() {
+		
+	}*/
 
-    public void sumarOro(Integer produccionOro) {
-        this.oro += produccionOro;
+	public void crearEntidadesIniciales(){
+		/*
+		Castillo castillo = new Castillo(this);
 
-    }
+		try {
+			castillo.construir(1, 1);
+		} catch (CasilleroLlenoException e) {
+			e.printStackTrace();
+		}
+		edificios.add(castillo);
 
-    public void cobrar(Integer monto) throws OroInsuficienteException, MontoErroneoError {
+		PlazaCentral plaza = new PlazaCentral(this);
 
-        if(monto < 0){
-            throw new MontoErroneoError("Se intentó hacer un cobro negativo. Algo salió horriblemente mal.");
-        }
+		try {
+			plaza.construir(5, 0);
+		} catch (CasilleroLlenoException e) {
+			e.printStackTrace();
+		}
+		edificios.add(plaza);
 
-        if (oro - monto < 0){
-            throw new OroInsuficienteException("El monto es mayor que los fondos actuales");
-        }
+		poblacion.add(new Aldeano(this));
+		poblacion.add(new Aldeano(this));
+		poblacion.add(new Aldeano(this));
 
-        oro -= monto;
-    }
+		poblacion.get(0).establecerCoordenadasDeNacimiento(5,3);
+		poblacion.get(1).establecerCoordenadasDeNacimiento(6,3);
+		poblacion.get(2).establecerCoordenadasDeNacimiento(7,0);*/
+	}
 
-    public void actualizar() {
-
-    }
-
-    public void crearEntidadesIniciales(Posicion posCastillo, Posicion posPlaza, Posicion posPriAldeano, Posicion posSegAldeano, Posicion posTerAldeano){
-        //Le pasa el mapa a los edificios para permitirles ubicar unidades con su posicion
-
-        Castillo castillo = new Castillo(this);
-
-        try {
-            castillo.construir(posCastillo.obtenerPosicionHorizontal(), posCastillo.obtenerPosicionVertical());
-        } catch (CasilleroLlenoException e) {
-            e.printStackTrace();
-        }
-        edificios.add(castillo);
-
-        PlazaCentral plaza = new PlazaCentral(this);
-
-        try {
-            plaza.construir(posPlaza.obtenerPosicionHorizontal(), posPlaza.obtenerPosicionVertical());
-        } catch (CasilleroLlenoException e) {
-            e.printStackTrace();
-        }
-        edificios.add(plaza);
-
-        poblacion.add(new Aldeano(this));
-        poblacion.add(new Aldeano(this));
-        poblacion.add(new Aldeano(this));
-
-        poblacion.get(0).establecerCoordenadasDeNacimiento(posPriAldeano.obtenerPosicionHorizontal(), posPriAldeano.obtenerPosicionVertical());
-        poblacion.get(1).establecerCoordenadasDeNacimiento(posSegAldeano.obtenerPosicionHorizontal(), posSegAldeano.obtenerPosicionVertical());
-        poblacion.get(2).establecerCoordenadasDeNacimiento(posTerAldeano.obtenerPosicionHorizontal(), posTerAldeano.obtenerPosicionVertical());
-    }
-
-    public void eliminarDeConstrucciones(Edificio edificio) {
-
-        if(!edificios.contains(edificio)){
-            throw new EdificioInexistenteEnConstruccionesException("No existe tal edificio");
-        }
-
-        edificios.remove(edificio);
-    }
 }
