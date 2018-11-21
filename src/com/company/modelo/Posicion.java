c9e368bb28279feb99ddbd264b8d6b52a22f2d7b
Martín Stefanelli <martin.stefanelli.96@hotmail.com>
2018-11-21 11:54:15 -0300

    Corrijo sintaxis de buscarEnemigosEnRadio

--- src/com/company/modelo/Posicion.java
+++ src/com/company/modelo/Posicion.java
@@ -2,28 +2,36 @@ package com.company.modelo;
 
 import com.company.excepciones.CasilleroLlenoException;
 import com.company.excepciones.CasilleroNoExistenteException;
-import com.company.excepciones.MapaLlenoException;
 import com.company.modelo.edificios.Edificio;
 import com.company.modelo.terreno.Mapa;
 import com.company.modelo.unidades.Unidad;
 
+import java.util.ArrayList;
+
 public class Posicion {
 
-    private Integer posicionHorizontal;
-    private Integer posicionVertical;
-    Mapa mapa = Mapa.getMapa();
+    private Integer posicionHorizontal, posicionVertical;
+    private Mapa mapa = Mapa.getMapa();
 
     public Posicion(int posicionHorizontal, int posicionVertical) {
         this.posicionHorizontal = posicionHorizontal;
         this.posicionVertical = posicionVertical;
     }
 
-    public void posicionar(Posicionable posicionable) throws CasilleroNoExistenteException, CasilleroLlenoException {
-        mapa.ubicar(posicionable, posicionHorizontal, posicionVertical);
+    public Integer obtenerPosicionHorizontal(){
+        return this.posicionHorizontal;
     }
 
-    public void colocarEnCasilleroLibreMasCercano(Unidad unaUnidad) throws MapaLlenoException {
+    public Integer obtenerPosicionVertical(){
+        return this.posicionVertical;
+    }
+
+    public void colocarEnCasilleroLibreMasCercano(Unidad unaUnidad) throws CasilleroLlenoException {
         mapa.colocarEnCasilleroLibreMasCercano(unaUnidad, this.posicionHorizontal, this.posicionVertical);
+    }
+
+    public void posicionar(Posicionable unPosicionable) throws CasilleroNoExistenteException, CasilleroLlenoException {
+        mapa.ubicar(unPosicionable, this.posicionHorizontal, this.posicionVertical);
     }
 
     public void eliminar() {
@@ -34,19 +42,31 @@ public class Posicion {
         }
     }
 
-    public boolean estaOcupado(int i, int j) {
-        return mapa.estaOcupado(i, j);
+    public boolean estaOcupado(Integer posicionHorizontal, Integer posicionVertical) {
+        return mapa.estaOcupado(posicionHorizontal, posicionVertical);
     }
 
-	public ArrayList buscarEnemigosEnRadio(Integer radio) {
-		ArrayList <Posicionable> posicionables = new ArrayList <>()
-		for(Integer i = posicionHorizontal - radio; i <= poscionHorizontal + radio; i++) {
-			for (Integer j = posicionVertical - radio; j <= posicionVertical + radio; j++) {
-				if (mapa.estaOcupado(i,j)) {
-					posicionables.add(mapa.conseguirOcupante(i,j));
-				}
-			}
-		}
-		return posicionables;
-	}
+    public ArrayList buscarEnemigosEnRadio(Integer radio) {
+        ArrayList <Posicionable> posicionables = new ArrayList <>();
+
+        for(Integer i = this.posicionHorizontal - radio; i <= this.posicionHorizontal + radio; i++) {
+
+            for (Integer j = this.posicionVertical - radio; j <= this.posicionVertical + radio; j++) {
+                try {
+
+                    if (mapa.estaOcupado(i, j)) {
+                        posicionables.add(mapa.conseguirOcupante(i, j));
+                    }
+
+                }
+                catch (CasilleroNoExistenteException e){
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        return posicionables;
+    }
+
 }
