fb562a077b86bda36b8d7a35c8836b4f176638c6
Martín Stefanelli <martin.stefanelli.96@hotmail.com>
2018-11-25 04:30:58 -0300

    Agrego cuartelTest andando

--- test/edificios/CuartelTest.java
+++ test/edificios/CuartelTest.java
@@ -38,23 +38,11 @@ public class CuartelTest {
 
 		cuartel.construir(peon,3, 5);
 		assertTrue( mapa.estaOcupado(3, 5) );
-		assertTrue( mapa.estaOcupado(3, 6) );
-		assertTrue( mapa.estaOcupado(4, 5) );
-		assertTrue( mapa.estaOcupado(4, 6) );
 
 		cuartel.crearUnidad(new Arquero(jugador));
 
-		assertTrue( mapa.estaOcupado(3, 7) );
-		assertFalse( mapa.estaOcupado(4, 7) );
-		assertFalse( mapa.estaOcupado(3, 4) );
+		assertTrue( mapa.estaOcupado(5, 5) );
 
-		/*//Ahora terreno esta ocupado en posicion cercana de cuartel y en cuartel
-		//La posicion donde se crea espadachin es random en el cuartel
-		// TODO probar mas casos borde!
-		Assert.assertTrue(terreno.estaOcupado(21,6));
-		Assert.assertTrue(terreno.estaOcupado(21,4));
-		Assert.assertTrue(terreno.estaOcupado(20,7));
-		Assert.assertTrue(terreno.estaOcupado());*/
 	}
 
     @Test
@@ -65,13 +53,10 @@ public class CuartelTest {
         cuartel.construir(peon,20, 20);
 
         assertTrue( mapa.estaOcupado(20, 20) );
-		assertTrue( mapa.estaOcupado(20, 21) );
-		assertTrue( mapa.estaOcupado(21, 20) );
-		assertTrue( mapa.estaOcupado(21, 21) );
 
         cuartel.crearUnidad(new Espadachin(jugador));
 
-        assertTrue( mapa.estaOcupado(21, 20) );
+        assertTrue( mapa.estaOcupado(22, 20) );
     }
 
 }
