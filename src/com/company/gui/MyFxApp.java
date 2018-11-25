94322b43c46d277eb0e0c929ef5face1272a0fb4
Juan <juangaray93@gmail.com>
2018-11-25 19:29:23 -0300

    Sumo Mapa y graficos

--- src/com/company/gui/MyFxApp.java
+++ src/com/company/gui/MyFxApp.java
@@ -1,8 +1,11 @@
 package com.company.gui;
 
+
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class MyFxApp extends Application {
@@ -12,9 +15,17 @@ public class MyFxApp extends Application {
         primaryStage.setTitle("TP2 Algoritmos III");
 
         Label label = new Label("Age of Empires II");
-        Scene scene = new Scene(label, 1200, 600);
-        primaryStage.setScene(scene);
 
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(200);
+        rectangle.setY(200);
+        rectangle.setWidth(200);
+        rectangle.setHeight(200);
+        Group root = new Group(rectangle);
+
+        Scene scene = new Scene(root, 1200, 600);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
---
+++ src/com/company/gui/terreno/Mapa.java
@@ -0,0 +1,28 @@
+package com.company.gui.terreno;
+
+import java.util.ArrayList;
+import javafx.scene.shape.Rectangle;
+
+public class Mapa {
+
+    private final Integer CASILLEROS_DE_ANCHO = 10;
+    private final Integer CASILLEROS_DE_ALTO = 10;
+    private final Integer DIMENSION = 10;
+    private ArrayList<Rectangle> casilleros = new ArrayList<>();
+    public void mostrarCasilleros(){
+
+
+        for(Integer y = 0; y < CASILLEROS_DE_ALTO; y++){
+            for(Integer x = 0; x < CASILLEROS_DE_ANCHO; x++){
+                Rectangle casillero = new Rectangle(x * DIMENSION, y * DIMENSION, DIMENSION,DIMENSION,);
+                casilleros.add(casillero);
+            }
+        }
+
+
+
+    }
+
+
+
+}
