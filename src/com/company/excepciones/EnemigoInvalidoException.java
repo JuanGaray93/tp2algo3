b48095747dfade713c9cfafad6bfc2355e349bc6
Juan Garay <jgaray@atixlabs.com>
2018-11-26 18:47:24 -0300

    agregue ataque y unidadAtacante

---
+++ src/com/company/excepciones/EnemigoInvalidoException.java
@@ -0,0 +1,6 @@
+package com.company.modelo;
+
+public class EnemigoInvalidoException extends Throwable {
+    public EnemigoInvalidoException(String s) {
+    }
+}
---
+++ src/com/company/modelo/Ataque.java
@@ -0,0 +1,4 @@
+package com.company.modelo;
+
+public class Ataque {
+}
